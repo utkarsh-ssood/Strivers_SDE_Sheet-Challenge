e445906f109ff05bdc65e310e20c320783989323
utkarsh-ssood <122226447+utkarsh-ssood@users.noreply.github.com>
2023-06-08 03:38:10 +0530

    Update and rename 01SetMatrixZeroes.cpp to 01_01SetMatrixZeroes.cpp

--- 01_01SetMatrixZeroes.cpp
+++ 01_01SetMatrixZeroes.cpp
@@ -1,3 +1,9 @@
+// 01 Array
+// 01.01-->Set matrix zeroes
+
+// Question->Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+// You must do it in place.
+
 #include <iostream>
 #include <vector>
 using namespace std;
