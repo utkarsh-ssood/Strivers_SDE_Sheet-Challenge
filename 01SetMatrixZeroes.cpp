c0f1bc7fbec517bf8474a0489932e72a16b635f1
utkarsh-ssood <122226447+utkarsh-ssood@users.noreply.github.com>
2023-06-08 03:35:06 +0530

    Add files via upload

---
+++ 01SetMatrixZeroes.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    void setZeroes(vector<vector<int>> &matrix)
+    {
+        int m = matrix.size(), n = matrix[0].size();
+        // vector<int>rows(m,0);
+        // vector<int>columns(n,0);
+        int col = 1;
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (matrix[i][j] == 0)
+                {
+                    matrix[i][0] = 0;
+                    if (j != 0)
+                        matrix[0][j] = 0;
+                    else
+                        col = 0;
+                }
+            }
+        }
+        for (int i = 1; i < m; i++)
+        {
+            if (matrix[i][0] == 0)
+            {
+                for (int j = 1; j < n; j++)
+                {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        for (int j = 1; j < n; j++)
+        {
+            if (matrix[0][j] == 0)
+            {
+                for (int i = 1; i < m; i++)
+                {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (matrix[0][0] == 0)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                matrix[0][i] = 0;
+            }
+        }
+        if (col == 0)
+        {
+            for (int i = 0; i < m; i++)
+            {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+};
+
+
+int main()
+{
+
+    return 0;
+}
